4f8f9620f5f795a401ac909d1a18261f648b854a
Nestnidev <neidernieto@unisangil.edu.co>
2024-04-11 14:12:38 -0500

    change dev structure bentogrid

---
+++ src/lib/city.ts
@@ -0,0 +1,31 @@
+export const CITY = [
+    {
+        class: "col-span-10 md:col-span-4",
+        title: "Tunja - Boyacá",
+        subTitle: "Asistencia legal",
+        img: "city/tunja.jpg",
+        description: "En mi práctica legal, priorizo la comunicación abierta y transparente con mis clientes. Mi objetivo es asegurarme de que comprendan completamente sus derechos legales y las opciones disponibles para resolver su situación legal de manera satisfactoria.",
+    },
+    {
+        class: "col-span-10 md:col-span-6",
+        title: "Chiquinquirá - Boyacá",
+        subTitle: "Asistencia legal",
+        img: "city/Chiquinquira_2.jpeg",
+        description: "Mi objetivo es asegurarme de que comprendan completamente sus derechoslegales y las opciones disponibles para resolver su situación legal demanera favorable. Trabajaré incansablemente para proteger sus interesesy buscar la mejor solución posible para su caso.",
+    },
+    
+    {
+        class: "col-span-10 md:col-span-6",
+        title: "Bogotá - Cundinamarca",
+        subTitle: "Asistencia legal",
+        img: "city/bogotá.webp",
+        description: "Además de mis servicios de representación legal, ofrezco talleres informativos y sesiones de asesoramiento gratuitas para ayudar a informar a nuestros ciudadanos sobre temas legales relevantes, desde sus derechos básicos hasta cómo navegar por el sistema legal con confianza.",
+    },
+    {
+        class: "col-span-10 md:col-span-4",
+        title: "Villa de Leyva",
+        subTitle: "Asistencia legal",
+        img: "city/villa de leyva.jpg",
+        description: "En mi práctica legal, priorizo la comunicación abierta y transparentecon mis clientes. Mi objetivo es asegurarme de que comprendancompletamente sus derechos legales y las opciones disponibles pararesolver su situación legal de manera satisfactoria.",
+    }
+]
