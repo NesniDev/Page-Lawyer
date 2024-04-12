897d045ac838a73c5a2f8ba9a86e73316e514617
Nestnidev <neidernieto@unisangil.edu.co>
2024-04-12 09:27:43 -0500

    Add section footer to the components Features

---
+++ src/lib/features.ts
@@ -0,0 +1,17 @@
+export const SERVICES = [
+    {
+      number: "8",
+      image: "anios.svg",
+      content: "Años de experiencia",
+    },
+    {
+      number: "1000",
+      image: "seguidores.svg",
+      content: "Seguidores en redes sociales",
+    },
+    {
+      number: "180",
+      image: "trabajos.svg",
+      content: "Servicios realizados",
+    },
+  ];
