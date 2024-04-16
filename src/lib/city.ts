d3333e7f1682c813a4ee63b60f8e57a0775008c0
Nestnidev <neidernieto@unisangil.edu.co>
2024-04-16 18:05:58 -0500

    Fix information citys

--- src/lib/city.ts
+++ src/lib/city.ts
@@ -2,30 +2,30 @@ export const CITY = [
     {
         class: "col-span-10 md:col-span-4",
         title: "Tunja - Boyacá",
-        subTitle: "Asistencia legal",
-        img: "city/tunja.jpg",
+        subTitle: "Y sus alrededores",
+        img: "city/tunja.avif",
         description: "En mi práctica legal, priorizo la comunicación abierta y transparente con mis clientes. Mi objetivo es asegurarme de que comprendan completamente sus derechos legales y las opciones disponibles para resolver su situación legal de manera satisfactoria.",
     },
     {
         class: "col-span-10 md:col-span-6",
         title: "Chiquinquirá - Boyacá",
-        subTitle: "Asistencia legal",
-        img: "city/Chiquinquira_2.jpeg",
+        subTitle: "Y sus alrededores",
+        img: "city/Chiquinquira.avif",
         description: "Mi objetivo es asegurarme de que comprendan completamente sus derechoslegales y las opciones disponibles para resolver su situación legal demanera favorable. Trabajaré incansablemente para proteger sus interesesy buscar la mejor solución posible para su caso.",
     },
     
     {
         class: "col-span-10 md:col-span-6",
         title: "Bogotá - Cundinamarca",
-        subTitle: "Asistencia legal",
+        subTitle: "Y sus alrededores",
         img: "city/bogotá.webp",
         description: "Además de mis servicios de representación legal, ofrezco talleres informativos y sesiones de asesoramiento gratuitas para ayudar a informar a nuestros ciudadanos sobre temas legales relevantes, desde sus derechos básicos hasta cómo navegar por el sistema legal con confianza.",
     },
     {
         class: "col-span-10 md:col-span-4",
-        title: "Villa de Leyva",
-        subTitle: "Asistencia legal",
-        img: "city/villa de leyva.jpg",
-        description: "En mi práctica legal, priorizo la comunicación abierta y transparentecon mis clientes. Mi objetivo es asegurarme de que comprendancompletamente sus derechos legales y las opciones disponibles pararesolver su situación legal de manera satisfactoria.",
+        title: "Barbosa - Santander",
+        subTitle: "Y sus alrededores",
+        img: "city/Barbosa.avif",
+        description: "Me encargo de representar a trabajadores y empleadores en casos relacionados con el ámbito laboral. Estos casos pueden involucrar disputas sobre despidos injustificados, discriminación, incumplimiento de contratos laborales, condiciones de trabajo, y otras cuestiones relacionadas con el empleo. ",
     }
 ]
