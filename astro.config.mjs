9addd4c2fc68c821fbd5cbab05144415cbf08bee
Nestnidev <neidernieto@unisangil.edu.co>
2024-04-23 17:22:50 -0500

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -8,5 +9,5 @@ export default defineConfig({
   site: "https://www.abogadasandraalba.com",
   integrations: [tailwind(), sitemap()],
   output: "server",
-  adapter: vercel(),
+  adapter: cloudflare(),
 });
