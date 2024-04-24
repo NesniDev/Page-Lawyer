02ffb26b4ba239e31ea54f05d0442717d2b428db
Nestnidev <neidernieto@unisangil.edu.co>
2024-04-24 09:46:52 -0500

    uninstall or install vercell

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.abogadasandraalba.com",
   integrations: [tailwind(), sitemap()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
