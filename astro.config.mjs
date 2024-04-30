ecdbd7bb168cb451584c8c96f94c4c81ae4d3ba6
Nestnidev <neidernieto@unisangil.edu.co>
2024-04-30 13:01:52 -0500

    install from pnpm preact, confetti, sweetalert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.abogadasandraalba.com",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), preact()],
   output: "server",
   adapter: vercel()
 });
